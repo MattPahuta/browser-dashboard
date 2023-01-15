66fb1acfd2ed5d3bc78b2a4d4bfa93778b52bb64
Matt Pahuta <mhpahuta@gmail.com>
2023-01-15 15:29:31 -0800

    Add initial getWeather location functions

--- app.js
+++ app.js
@@ -44,6 +44,28 @@ function updateTime() {
   document.querySelector('#time').textContent = currentTime;
 }
 
+// get/display weather info
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  const crd = pos.coords;
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+
 // add content to DOM:
 getRandomBg(); // make this once a day?
 setInterval(updateTime, 1000);
